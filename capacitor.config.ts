8c6b26ce47c76a73ed65c013b34cf11397ad0ab3
Juan Martínez <88853083+Ishigaming@users.noreply.github.com>
2024-10-07 02:37:57 -0500

    Primer commit: Subida inicial del proyecto

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'FBTaller',
+  webDir: 'www'
+};
+
+export default config;
